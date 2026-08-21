3b4d6baf6b16f481417684e8b519b460e3cf2dc3
Prince <shopiwolf99@gmail.com>
2026-08-21 21:43:09 +0530

    Create MembershipPage component with content and navigation

---
+++ app/membership/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { MembershipContent } from '@/components/membership-content'
+import { BottomNav } from '@/components/bottom-nav'
+
+export default function MembershipPage() {
+  return (
+    <>
+      <MembershipContent />
+      <BottomNav />
+    </>
+  )
+}
