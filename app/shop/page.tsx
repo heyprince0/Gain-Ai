8cda9d388c8625da1486c45f912d7a2a9d7ccf8e
Prince <shopiwolf99@gmail.com>
2026-08-21 21:54:39 +0530

    Create ShopPage component with ShopContent and BottomNav

---
+++ app/shop/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { ShopContent } from '@/components/shop-content'
+import { BottomNav } from '@/components/bottom-nav'
+
+export default function ShopPage() {
+  return (
+    <>
+      <ShopContent />
+      <BottomNav />
+    </>
+  )
+}
